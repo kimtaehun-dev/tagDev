22426fa4c6d628c3e15486b7ec6cc587554cd4a2
kimtaehun <kth_markany@naver.com>
2024-10-04 13:59:21 +0900

    헤더 반응형 디자인

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,12 +1,32 @@
 
+import { useEffect, useState } from 'react';
 import HeaderTittle from '../../atoms/tittle/HeaderTittle';
 import './Header.css'
 
 export default function Header(){
   const headerContentsList = ['About me','Skills', 'Archiving','Projects','Career'];
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(()=>{
+    const handleScroll = ()=>{
+      if(window.scrollY>0){
+        setIsScrolled(true)
+      }
+      else{
+        setIsScrolled(false);
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    return ()=>{
+      window.removeEventListener('scroll', handleScroll);
+    }
+
+  },[])
+
   return(
-    <header>
-      <div className='header-contents'>
+    <header style={{backgroundColor: isScrolled?'beige':''}}>
+      <div className={isScrolled?'scrolled-header header-common':'noScrolled-header header-common'}>
         <HeaderTittle />
         <div className='header-menu-group'>
           {headerContentsList.map((item, index)=>(
--- src/components/organisms/main/MainSection.tsx
+++ src/components/organisms/main/MainSection.tsx
@@ -1,10 +1,12 @@
+import './mainSection.css';
+
 export default function MainSection(){
   return(
     <main>
-      <div>아티클1</div>
-      <div>아티클2</div>
-      <div>아티클3</div>
-      <div>아티클4</div>
+      <div className='test'>아티클1</div>
+      <div className='test'>아티클2</div>
+      <div className='test'>아티클3</div>
+      <div className='test'>아티클4</div>
     </main>
   )
 }
