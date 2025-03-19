768fb07c866b5cd957d0884159cc755e482bd134
kimtaehun <kth_markany@naver.com>
2025-03-19 22:37:25 +0900

    인트로듀스 아티클 아이탬 컴포넌트 추가1

--- src/components/organisms/article/introduce/Introduce.tsx
+++ src/components/organisms/article/introduce/Introduce.tsx
@@ -2,9 +2,12 @@ import ArticleTittle from '../../../molecules/articleTittle/ArticleTittle'
 import './Introduce.css'
 export default function Introduce(){
   return(
-    <div className="introduce-article">
+    <div className="introduce-frame">
       <div className="introduce-container">
         <ArticleTittle tittleText={"Introduce"}/>
+        <div className='introduce-main'>
+
+        </div>
       </div>
     </div>
   )
