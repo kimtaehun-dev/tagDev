b42e8b52619ed0d351edeb2a702430f4e06e0add
kth_HOME <changetodev@gmail.com>
2025-03-19 22:40:50 +0900

    Revert "인트로듀스 아티클 아이탬 컴포넌트 추가1"
    This reverts commit 768fb07c866b5cd957d0884159cc755e482bd134.

--- src/components/organisms/article/introduce/Introduce.tsx
+++ src/components/organisms/article/introduce/Introduce.tsx
@@ -2,12 +2,9 @@ import ArticleTittle from '../../../molecules/articleTittle/ArticleTittle'
 import './Introduce.css'
 export default function Introduce(){
   return(
-    <div className="introduce-frame">
+    <div className="introduce-article">
       <div className="introduce-container">
         <ArticleTittle tittleText={"Introduce"}/>
-        <div className='introduce-main'>
-
-        </div>
       </div>
     </div>
   )
