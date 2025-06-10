75490ffdb3a7772dbeac107ea51d1290b7523ff3
TaeHunKim <changetodev@gmail.com>
2025-06-10 17:41:31 +0900

    업데이트 일자 추가가

--- src/components/organisms/footer/Footer.tsx
+++ src/components/organisms/footer/Footer.tsx
@@ -3,6 +3,7 @@ import './Footer.css';
 export default function Footer(){
   return (
     <footer>
+      <p>last updated : 2025.06.10</p>
       <p>Copyright© 2024. Tae Hun Kim. All rights reserved</p>
     </footer>
   )
